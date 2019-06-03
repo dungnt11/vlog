fe42496aad540d7bbf25b04d0af212761ee1f087
nguyen dung <nihilism.core@gmail.com>
2019-06-03 23:30:21 +0700

    can fix email da dang ki

--- routes/users/test.js
+++ routes/users/test.js
@@ -1,25 +1,3 @@
-const express = require("express");
-const router = express.Router();
-const jwt = require("jsonwebtoken");
-const multer = require("multer");
-const path = require("path");
-
-const authJwt = require("../../midderware/jwtAuth");
-const db = require("../../model/users");
-const checkRegister = require("../../validators/users/register");
-const checkLogin = require("../../validators/users/login");
-const jwtConfig = require("../../config/jwtToken");
-
-// kiểm tra dữ liệu login ( midderware )
-const checkValueLogin = (req, res, next) => {
-  const { isError, err } = checkLogin(req.body);
-  if (isError) {
-    res.status(401).json({ err });
-  } else {
-    next();
-  }
-};
-
 // --------------------------- REGISTER ----------------------
 
 function randomName() {
@@ -109,48 +87,3 @@ router.post("/register", upload.single("file"), _midErr, (req, res) => {
     }
   });
 });
-
-/**
- * @Route POST /login
- * @Desc  login user return jwt-token
- * @Access Private
- */
-router.post("/login", checkValueLogin, (req, res) => {
-  const { email, pwd } = req.body;
-  // tim email
-  db.findOne({ email })
-    .then(user => {
-      if (user) {
-        // so sanh mat khau
-        let _db = new db();
-        _db.dehash(pwd, user.pwd, done => {
-          if (!done) {
-            res.status(404).json({ pwd: "Mật khẩu sai" });
-          } else {
-            const payload = {
-              id: user.id,
-              name: user.name,
-              avatar: user.avatar
-            };
-            // add jwt
-            let token = jwt.sign(payload, jwtConfig.secret, {
-              expiresIn: Math.floor(Date.now() / 1000) + jwtConfig.expires
-            }); // expires : 1 hour
-            res.json({ token: "Bearer " + token });
-          }
-        });
-      } else {
-        // neu email trung khop
-        res.status(404).json({ email: "Email không đúng" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ err: "Lỗi không xác định" + err });
-    });
-});
-// test
-router.get("/hello", authJwt, (req, res) => {
-  res.json({ msg: req.user });
-});
-
-module.exports = router;
